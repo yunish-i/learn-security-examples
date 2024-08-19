e5b4bea97d34e070f97aac709e9bd27eacf444a3
Joydeep Mitra <joy@Joydeeps-MacBook-Pro.local>
2024-08-19 12:34:00 -0400

    ts changes

--- spoofing/mal.ts
+++ spoofing/mal.ts
@@ -1,12 +1,10 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
+import express, { Request, Response } from "express";
 const app = express();
-const bodyParser = require('body-parser');
 
 //app.use(cookieParser());
 
 // Assume the server is malicious. Doesn't do anything useful. Only changes cookies.
-app.get("/malhome", (req, res) => {
+app.get("/malhome", (req: Request, res: Response) => {
   res.send(`<h1> You Won! </h1><script> console.log("Session = " + document.cookie); </script>`)
 })
 
